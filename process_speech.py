4717bde27e2cbe8b7924c70d3a2cb495ab5084ab
Mauricio <mau.altgr@hotmail.com>
2019-03-25 17:16:33 -0300

    clean process code

--- process_speech.py
+++ process_speech.py
@@ -1,8 +1,9 @@
 import numpy as np
-from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
 import unidecode
 import pdb
+import re
+from tqdm import tqdm
 
 
 def cleanText(x):
@@ -11,36 +12,42 @@ def cleanText(x):
 
 
 def cleanSentence(x):
+	c = []
 	clean_x = unidecode.unidecode(x).lower()
 	clean_x = clean_x.replace(",", " , ").replace(":", " : ").replace("?", " ? ").replace("¿", " ¿ ")
 	clean_x = clean_x.replace("\"", " ,, ").replace(";", " ; ").replace("!", " ! ").replace("¡", " ¡ ")
 	clean_x = clean_x.replace("\'", " ,, ").replace("(", " ( ").replace(")", " ) ").replace("-", " - ")
 	clean_x = clean_x.replace("...", ' ... ').replace("%", " % ").replace("*****", " ***** ")
-	clean_x += " eos "
-	return clean_x
+	for i in clean_x.split(" "):
+		c.append(num) if bool(re.search(r"\d", i)) else c.append(i)
+	c.append(eos)
+	return c
 
 
-le = LabelEncoder()
-dis = np.load("discursos.npy")
-sen = []
-numsen = []
-print("{} speeches".format(len(dis)))
-nsentences = 0
-long_sentence = ""
-for i in dis:
-	clean_speech = cleanText(i)
-	nsentences += len(clean_speech)
-	for sentence in clean_speech:
-		clean_sentence = cleanSentence(sentence)
-		sen.append(clean_sentence)
-		long_sentence += clean_sentence
-le.fit(long_sentence.split())
-print("{} sentences".format(nsentences))
-print("{} features".format(len(le.classes_)))
-np.save("labels", le.classes_)
-for i, ss in enumerate(sen):
-	print("{}/{}".format(i, nsentences))
-	numsen.append(le.transform(ss.split()))
-	if i%1000==0:
-		np.save("num_sentences", numsen)
-np.save("num_sentences", numsen)
+if __name__=="__main__":
+	le = LabelEncoder()
+	dis = np.load("discursos.npy")
+	sen = []
+	numsen = []
+	print("{} speeches".format(len(dis)))
+	unique = []
+	pad = "<PAD>"
+	eos = "<EOS>"
+	num = "<NUM>"
+	for i in dis:
+		clean_speech = cleanText(i)
+		for sentence in clean_speech:
+			cleaned_sentence = cleanSentence(sentence)		
+			sen.append(cleaned_sentence)
+			unique += list(set(cleaned_sentence))
+	unique = list(set(unique))
+	unique.insert(0, pad)
+	le.fit(unique)
+	print("{} sentences".format(len(sen)))
+	print("{} features".format(len(le.classes_)))
+	np.save("labels", le.classes_)
+	for ss in tqdm(sen):
+		numsen.append(le.transform(ss))
+		if i%1000==0:
+			np.save("num_sentences", numsen)
+	np.save("num_sentences", numsen)
